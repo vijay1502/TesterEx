999d35c0d97253d19ef692750a7ae76bfa7cda8f
vijay1502 <pruthvikrishna97@gmail.com>
2025-01-07 11:32:38 +0530

    Init-1 Commit

---
+++ src/main/java/com/Trial2/TesterTrail/TesterTrailApplication.java
@@ -0,0 +1,13 @@
+package com.Trial2.TesterTrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesterTrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Trial2/TesterTrail/TesterTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trial2.TesterTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
